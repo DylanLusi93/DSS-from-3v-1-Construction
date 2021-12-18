eb465f9a3adc858130d85155588c9bf0e0c532ae
DylanLusi93 <96262665+DylanLusi93@users.noreply.github.com>
2021-12-18 12:19:09 -0700

    Add files via upload

--- Three_v_plus_one_construction.py
+++ Three_v_plus_one_construction.py
@@ -4,20 +4,21 @@ def three_v_plus_one_construction(v, s_2_4_v):
      of order 2v + 1) and S(2,4,v). s_2_4_v must be a list of quadruples on point set [0, v-1]. The resulting points
      of the output S(2,4,u = 3v + 1) are labelled such that its MinSum is at least (4*u + 2)/3."""
 
-    s_2_4_3v_plus_1 = [[] for _ in range(3*v*(3*v + 1)//12)]
+    s_2_4_3v_plus_1 = []
     kts_order = 2*v + 1
     als_order = kts_order//3
     if als_order % 18 in [3, 15] and als_order > 15 and als_order % 5 != 0:
         # force s_2_4_v onto point set [2v + 1, 3v]
-        for quadruple in s_2_4_v:
-            quadruple += kts_order
+        for idx, quadruple in enumerate(s_2_4_v):
+            quadruple = [x + kts_order for x in quadruple]
+            s_2_4_v[idx] = quadruple
         bats = BR.bats_from_br(als_order)
         for x in range(2*v + 1, 3*v + 1):
-            for triple in bats[x - 2*v + 1]:
+            for triple in bats[x - (2*v + 1)]:
                 s_2_4_3v_plus_1 = s_2_4_3v_plus_1 + [[x] + triple]
 
     else:
         print("Error: Invalid v. (2v + 1)/3 must be congruent to 3 or 15 mod 18, must be greater than 15, and must not"
               " be divisible by 5.")
-
+    s_2_4_3v_plus_1 = s_2_4_3v_plus_1 + s_2_4_v
     return s_2_4_3v_plus_1
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import Three_v_plus_one_construction as TVP1
 import sys
 
 if __name__ == '__main__':
+    # Note: testFile contains an ingredient S(2,4,76) that you may use to test the program; if you
+    # do opt to do this, set u (number of files) to 76*3 + 1 = 229
+
     u = input("Enter the number of files u in the storage system: ")
     u = int(u)
     if u % 108 in [13, 40]:
@@ -25,8 +28,12 @@ if __name__ == '__main__':
 
         s24v_ingred.pop()
 
+        if not Test.is_s24v(v, s24v_ingred):
+            print("Error: The provided putative ingredient S(2,4,v) is not an S(2,4,v)!")
         # get the storage system ss, represented as an S(2,4,u)
         ss = TVP1.three_v_plus_one_construction(v, s24v_ingred)
+        if not Test.is_s24v(3*v + 1, ss):
+            print("OH NOOOOOOO")
         print("Here is the resulting storage system on", u, "points, having guaranteed MinSum at least (4*u + 2)/3 =", (4*u + 2)//3, ":")
         for block in ss:
             print(block)
